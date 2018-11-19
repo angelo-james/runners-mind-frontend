a632cd648ea8d854f38b1ea790bd0538e708ccef
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-11-19 13:21:05 -0800

    imported react-router-dom bootstrap and components for loginform navbar footer and home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import NavBar from './Common/NavBar';
+import LoginForm from './Components/LoginForm';
+import HomePage from './Components/HomePage';
+import Footer from './Common/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>TEST TEST TEST</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/*" component={ NavBar } />
+          <Route exact path="/" component={ () => <Redirect to="/login" /> } />
+          <Route exact path="/login" component={ LoginForm } />
+          <Route exact path="/home" component={ HomePage } />
+        </div>
+      </Router>
     );
   }
 }
