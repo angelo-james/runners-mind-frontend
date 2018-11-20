2ccbe42720cbc07c336e47ee40919b8196639e1d
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-11-20 13:13:42 -0800

    imported font-awesome and added route for footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
+import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -18,6 +19,7 @@ class App extends Component {
           <Route exact path="/" component={ () => <Redirect to="/login" /> } />
           <Route exact path="/login" component={ LoginForm } />
           <Route exact path="/home" component={ HomePage } />
+          <Route exact path="/*" component={ Footer } />
         </div>
       </Router>
     );
