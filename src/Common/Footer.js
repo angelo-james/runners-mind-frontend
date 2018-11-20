9b0ca3806605583ffef2c7b6ebbdba3dfa1cadc2
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-11-20 13:15:49 -0800

    imported reactstrap created navbar for footer

--- src/Common/Footer.js
+++ src/Common/Footer.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react'
 
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
 class Footer extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="mt-5">
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/">Runners Mind</NavbarBrand>
+          <NavbarToggler />
+          <Collapse navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  NavLink
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Home Page
+                  </DropdownItem>
+                  <DropdownItem>
+                    Posts
+                  </DropdownItem>
+                  <DropdownItem>
+                    Friends
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
       </div>
     )
   }
