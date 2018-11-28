ec9ec361fca830efbe4e6ef81eca772b370cbcda
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-11-27 16:14:48 -0800

    added about section and background image behind the form

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -12,20 +12,49 @@ import {
 class LoginForm extends Component {
   render() {
     return (
-      <Container
-        className="mx-auto text-center"
-      >
-        <Form className="container border mt-5 w-50">
+      // <Container
+      // className="mx-auto text-center"
+      // >
+      <div className="text-center">
+        <img className="login-form-image" src="https://source.unsplash.com/1600x900/?peoplerunning" alt=""></img>
+        <Form color="dark" className="container mt-5 w-25 login-form">
           <h1 className="text-center">Login</h1>
           <FormGroup>
             <Label for="email">Enter Email</Label>
-            <Input pattern="[A-Za-z]"required/>
+            <Input required/>
             <Label for="password">Enter Password</Label>
             <Input type="password" required/>
             <Button className="mt-2" color="dark">Submit</Button>
           </FormGroup>
         </Form>
-      </Container>
+        {/* <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div> */}
+        <div className="mt-3">
+          <div className="info-message row text-center mx-auto">
+            <div className="info-message container col-4 text-center">
+              <img className="login-about-image" src="https://via.placeholder.com/250x200" alt="placeholder"></img>
+              <h3 className="mt-3">Community</h3>
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in eros nec diam semper porta vitae id ante. Proin maximus laoreet mauris faucibus hendrerit. Praesent.
+              </p>
+            </div>
+            <div className="info-message container col-4 text-center">
+              <img className="login-about-image" src="https://via.placeholder.com/250x200" alt="placeholder"></img>
+              <h3 className="mt-3">Goals</h3>
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in eros nec diam semper porta vitae id ante. Proin maximus laoreet mauris faucibus hendrerit. Praesent.
+              </p>
+            </div>
+            <div className="info-message container col-4 text-center">
+              <img className="login-about-image" src="https://via.placeholder.com/250x200" alt="placeholder"></img>
+              <h3 className="mt-3">Progress</h3>
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in eros nec diam semper porta vitae id ante. Proin maximus laoreet mauris faucibus hendrerit. Praesent.
+              </p>
+            </div>
+          </div>
+        </div>
+      {/* </Container> */}
+      </div>
     )
   }
 }
