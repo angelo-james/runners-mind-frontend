4dced37c37e81e447ecf80db89d88b8f4eeb0cfb
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-11-29 15:41:57 -0800

    created file for user actions

---
+++ src/Actions/userActions.js
@@ -0,0 +1,10 @@
+import { GET_USER, ADD_USER, DELETE_USER, VALIDATE_USER } from './types';
+
+import axios from 'axios';
+
+export const validateUser = email => {
+  // axios.post
+  //.catch
+        //payload: //result from the post request'
+  
+}
