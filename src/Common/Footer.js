79d2e89bce03120ee8d8129598ac81adc28fd217
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-11-29 15:45:41 -0800

    updated footer component

--- src/Common/Footer.js
+++ src/Common/Footer.js
@@ -11,32 +11,23 @@ import {
 class Footer extends Component {
   render() {
     return (
-      <div className="mt-5">
-        <Navbar color="dark" dark expand="sm" className="mb-5">
-          <Container>
-            <NavbarBrand href='/'>
-              Runners Mind
-            </NavbarBrand>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="https://github.com/angelo-james">
-                  <i class="fa fa-github fa-2x" aria-hidden="true"></i>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink email="angelojames.arriola@gmail.com">
-                  <i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://linkedin.com/in/angelo-james-arriola">
-                  <i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
-                </NavLink>
-              </NavItem>
-            </Nav>
-          </Container>
+      // <Container className="ml-auto">
+        <Navbar color="dark" dark className="fixed-bottom">
+
+            <NavLink href="https://github.com/angelo-james">
+              <i className="fa fa-github icon">GITHUB</i>
+            </NavLink>
+        
+            <NavLink href="mailto:angelojames.arriola@gmail.com">
+              <i className="fa fa-envelope-o icon">GMAIL</i>
+            </NavLink>
+          
+            <NavLink href="https://linkedin.com/in/angelo-james-arriola">
+              <i className="fa fa-linkedin-square icon">LINKEDIN</i>
+            </NavLink>
+              
         </Navbar>
-      </div>
+      // </Container>
     )
   }
 }
