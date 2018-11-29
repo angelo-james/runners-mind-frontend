e7a2e73d79de08597fe4125c438c6210bd40979d
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-11-29 15:44:40 -0800

    imported sign up form component and created route

--- src/App.js
+++ src/App.js
@@ -7,25 +7,26 @@ import './App.css';
 
 import NavBar from './Common/NavBar';
 import LoginForm from './Components/LoginForm';
+import SignUpForm from './Components/SignUpForm';
 import HomePage from './Components/HomePage';
 import Footer from './Common/Footer';
 
 import { Provider } from 'react-redux';
 import store from './store';
-
 class App extends Component {
   render() {
     return (
-      <Provider store={ store }>
-        <Router>
-          <div className="App">
-            <Route exact path="/*" component={ NavBar } />
-            <Route exact path="/" component={ () => <Redirect to="/login" /> } />
-            <Route exact path="/login" component={ LoginForm } />
-            <Route exact path="/home" component={ HomePage } />
-            <Route exact path="/*" component={ Footer } />
-          </div>
-        </Router>
+<Provider store={ store }>
+      <Router>
+        <div className="App">
+          <Route exact path="/*" component={ NavBar } />
+          <Route exact path="/" component={ () => <Redirect to="/login" /> } />
+          <Route exact path="/login" component={ LoginForm } />
+          <Route exact path="/signup" component={ SignUpForm } />
+          <Route exact path="/home" component={ HomePage } />
+          <Route exact path="/*" component={ Footer } />
+        </div>
+      </Router>
       </Provider>
     );
   }
