ab1986d8a7998e0abc453b5dc04dc91347772ba6
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-11-30 09:54:03 -0800

    created case for creating user and validating user success or failed

--- src/Reducers/userReducer.js
+++ src/Reducers/userReducer.js
@@ -1,15 +1,40 @@
-import { GET_USER, ADD_USER, DELETE_USER, VALIDATE_USER } from '../Actions/types'
+import { 
+  GET_USER, 
+  ADD_USER,
+  ADD_USER_FAILED, 
+  DELETE_USER, 
+  VALIDATE_USER, 
+  VALIDATE_USER_FAILED } from '../Actions/types'
 
 const initalState = {
-  user: []
+  user: {},
+  loginSuccess: null
 }
 
  export default function(state = initalState, action) {
   switch(action.type) {
     case VALIDATE_USER:
-      this.setState({ user: action })
       return {
-        ...state
+        ...state,
+        user: action.payload,
+        loginSuccess: true
+      }
+    case VALIDATE_USER_FAILED:
+      return {
+        ...state,
+        user: action.payload,
+        loginSuccess: false
+      }
+    case ADD_USER:
+      return {
+        ...state,
+        user: action.payload
+      }
+    case ADD_USER_FAILED:
+      return {
+        ...state,
+        user: action.payload,
+        loginSuccess: false
       }
     default:
       return state;
