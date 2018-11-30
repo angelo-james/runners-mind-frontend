1f84ed4d0803146b119b9d0da28ea5ca266e7bfe
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-11-30 09:44:18 -0800

    removed axios, created to function using fetch http requests

--- src/Actions/userActions.js
+++ src/Actions/userActions.js
@@ -1,10 +1,57 @@
-import { GET_USER, ADD_USER, DELETE_USER, VALIDATE_USER } from './types';
+import { 
+  GET_USER, 
+  ADD_USER,
+  ADD_USER_FAILED, 
+  DELETE_USER, 
+  VALIDATE_USER, 
+  VALIDATE_USER_FAILED } from './types';
 
-import axios from 'axios';
+export const validateUser = credentials => {
+  return dispatch => {
+    fetch('/users/login', {
+      method: 'POST',
+      body: JSON.stringify(credentials),
+      headers: { 
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then( async result => {
+      dispatch({
+        type: VALIDATE_USER,
+        payload: await result.json()
+      })
+    })
+    .catch(error => {
+      return({
+        type: VALIDATE_USER_FAILED,
+        payload: error
+      })
+    })
+  }  
+}
 
-export const validateUser = email => {
-  // axios.post
-  //.catch
-        //payload: //result from the post request'
-  
+export const createUser = payload => {
+  return dispatch => {
+    fetch('/users', {
+      method: 'POST',
+      body: JSON.stringify(payload),
+      headers: { 
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(async result => {
+      dispatch({
+        type: ADD_USER,
+        payload: await result.json()
+      })
+    })
+    .catch(error => {
+      return({
+        type: ADD_USER_FAILED,
+        payload: error
+      })
+    })
+  }
 }
