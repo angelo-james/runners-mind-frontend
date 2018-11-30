6da75d784c3a21359736fdd3a3ae6b09e8af23d1
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-11-30 09:42:32 -0800

    added types for failed user creation and validation

--- src/Actions/types.js
+++ src/Actions/types.js
@@ -1,5 +1,6 @@
 export const GET_USER = 'GET_USER';
 export const ADD_USER = 'ADD_USER';
+export const ADD_USER_FAILED = 'ADD_USER_FAILED';
 export const DELETE_USER = 'DELETE_USER';
 export const VALIDATE_USER = 'VALIDATE_USER';
-
+export const VALIDATE_USER_FAILED = 'VALIDATE_USER_FAILED';
