f0c59c70178bb8178d088de642a41f80ff781f7f
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-11-30 09:41:01 -0800

    imported redux-logger as middleware and bug fixed redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import ReduxThunk from 'redux-thunk';
 import rootReducer from './Reducers/rootReducer';
+import logger from 'redux-logger';
 
 //initial state that gets passsed down as props to all the other components
 const initialState = {}
 
 //storing thunk under this variable middleware
- const middleware = [thunk];
+ const middleware = [ReduxThunk, logger];
 
  //creating the stroe and passing the root reducer and the initial state also applying thunk as middleware
  const store = createStore(
