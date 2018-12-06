168732a79e3380f193df99b9c5ba9532bd7f6ca8
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-12-06 11:40:20 -0800

    imported react router dom and removed stats and progress links

--- src/Common/NavBar.js
+++ src/Common/NavBar.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
 
 import {
   Collapse,
@@ -9,11 +8,9 @@ import {
   Nav,
   NavItem,
   NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
   Container } from 'reactstrap';
+
+  import { Link } from 'react-router-dom';
 
 class NavBar extends Component {
   constructor(props) {
@@ -32,30 +29,29 @@ class NavBar extends Component {
   render() {
     return (
       <div>
-        <Navbar color="dark" dark expand="lg" className="fixed-top">
-          <Container>
-            <NavbarBrand href='/'>
-              Runners Mind
-            </NavbarBrand>
-            <NavbarToggler onClick={ this.toggle } />
-            <Collapse isOpen={ this.state.isOpen } navbar>
-              <Nav className="ml-auto" navbar>
-                <NavItem>
-                  <NavLink href='/home'>Home</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href='/home'>Stats</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href='/home'>Progress</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href='/home'>Sign out</NavLink>
-                </NavItem>
-              </Nav>
-            </Collapse>
-          </Container>
-        </Navbar>
+          <Navbar color="dark" dark expand="lg" className="fixed-top">
+            <Container>
+              <Link to='/'>
+                <NavbarBrand>
+                  Runners Mind
+                </NavbarBrand>
+              </Link>
+              <NavbarToggler onClick={ this.toggle } />
+              <Collapse isOpen={ this.state.isOpen } navbar>
+                <Nav className="ml-auto" navbar>
+                  <NavItem>
+                    <Link to="/home"><NavLink>Home</NavLink></Link>
+                  </NavItem>
+                  <NavItem>
+                    <Link to='/runners'><NavLink>Find Runners</NavLink></Link>
+                  </NavItem>
+                  <NavItem>
+                    <Link to='/login'><NavLink>Sign out</NavLink></Link>
+                  </NavItem>
+                </Nav>
+              </Collapse>
+            </Container>
+          </Navbar>
       </div>
     )
   }
