48016dcca0d2a0d5aa3df5ba6f933aa5155ae903
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-12-06 17:34:08 -0800

    imported connects added mapstatetoprops

--- src/Components/UserProfileCard.js
+++ src/Components/UserProfileCard.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class UserProfileCard extends Component {
   render() {
     return (
       <div className="container">
-        <div className="col-3 card w-25 home-page-user mx-auto">
+        <div className="card home-page-user mx-auto">
           <img className="avatar-image mt-3" src="https://source.unsplash.com/1600x900/?people" alt="placeholder"></img>
-          <h5 className="card-title text-center mt-3">John Doe</h5>
+          <h5 className="card-title text-center mt-3">{this.props.user.user.username.toUpperCase()}</h5>
           <hr></hr>
           <div className="row text-center">
             <div className="col pl-5">
               <p>Following</p>
-              <p>25</p>
+              <p>{this.props.user.user.following.length}</p>
             </div>
             <div className="col pr-5">
               <p>Followers</p>
-              <p>21</p>
+              <p>{this.props.user.user.followers.length}</p>
             </div>
           </div>
-          <small className="text-center">Date Joined</small>
+          <small className="text-center">{this.props.user.user.date}</small>
         </div>
       </div>
     )
   }
 }
 
-export default UserProfileCard;
+const mapStateToProps = state => {
+  return {
+    ...state.user
+  }
+}
+
+export default connect(mapStateToProps, null)(UserProfileCard);
